66576d27b3907aa0080e1b7cf60c922296f7d74b
eeboulanger <eboulanger.pro@gmail.com>
2024-08-01 14:21:14 +0200

    Added get patient by id method in patient service and http error handling

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {PatientComponent} from './components/patient/patient.component';
+import {PatientListComponent} from './components/patient-list/patient-list.component';
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, PatientComponent],
+  imports: [CommonModule, PatientListComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- front-end/src/app/services/patient.service.ts
+++ front-end/src/app/services/patient.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Patient} from "../models/Patient";
-import {Observable} from "rxjs";
+import {catchError, Observable, throwError} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +10,30 @@ export class PatientService {
 
   private apiUrl = 'http://localhost:8080/patients'; //Gateway url
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getPatients(): Observable<Patient[]> {
-    return this.http.get<Patient[]>(this.apiUrl);
+    return this.http.get<Patient[]>(this.apiUrl)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  getPatientById(id: string): Observable<Patient> {
+    return this.http.get<Patient>(`${this.apiUrl}/${id}`)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.status === 404) {
+      errorMessage = 'Patient not found';
+    } else {
+      errorMessage = 'An error occurred: ' + error.message;
+    }
+    return throwError(() => new Error(errorMessage));
   }
 }
