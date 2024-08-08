7b39f4045498bc36b2b82967e707cff0700777b2
eeboulanger <eboulanger.pro@gmail.com>
2024-08-08 15:16:39 +0200

    Initialized risk evaluator microservice

---
+++ risk-evaluator/src/main/java/com/medilabo/risk_evaluator/RiskEvaluatorApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.risk_evaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskEvaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskEvaluatorApplication.class, args);
+	}
+
+}
---
+++ risk-evaluator/src/test/java/com/medilabo/risk_evaluator/RiskEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.risk_evaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskEvaluatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
