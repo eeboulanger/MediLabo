77ceb411d4907e7b04883fc651ada966deffaa15
eeboulanger <eboulanger.pro@gmail.com>
2024-08-11 16:47:34 +0200

    Added Eureka and actuator in all microservices

--- medical-recordservice/src/main/java/com/medilabo/medicalrecords/MedicalRecordsApplication.java
+++ medical-recordservice/src/main/java/com/medilabo/medicalrecords/MedicalRecordsApplication.java
@@ -2,8 +2,10 @@ package com.medilabo.medicalrecords;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MedicalRecordsApplication {
 
 	public static void main(String[] args) {
--- risk-evaluator/src/main/java/com/medilabo/riskevaluator/RiskEvaluatorApplication.java
+++ risk-evaluator/src/main/java/com/medilabo/riskevaluator/RiskEvaluatorApplication.java
@@ -2,10 +2,12 @@ package com.medilabo.riskevaluator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class RiskEvaluatorApplication {
 
 	public static void main(String[] args) {
