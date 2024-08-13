2ebd9de11c3ad9d7624fc1a49f220f7553c26b85
eeboulanger <eboulanger.pro@gmail.com>
2024-08-13 14:38:49 +0200

    Added documentation on endpoints using openApi 3 and swagger UI

--- medical-recordservice/src/main/java/com/medilabo/medicalrecords/controller/MedicalRecordController.java
+++ medical-recordservice/src/main/java/com/medilabo/medicalrecords/controller/MedicalRecordController.java
@@ -2,6 +2,10 @@ package com.medilabo.medicalrecords.controller;
 
 import com.medilabo.medicalrecords.model.MedicalRecord;
 import com.medilabo.medicalrecords.service.IMedicalRecordService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +20,23 @@ public class MedicalRecordController {
     private IMedicalRecordService medicalRecordService;
 
     @GetMapping("/{patientId}")
+    @Operation(summary = "Fetch medical records for patient by patient id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found records for patient",
+                    content = {
+                            @Content(mediaType = "application/json")})
+    })
     public ResponseEntity<List<MedicalRecord>> getMedicalRecords(@PathVariable String patientId) {
         return ResponseEntity.ok(medicalRecordService.findByPatientId(patientId));
     }
 
     @PostMapping
+    @Operation(summary = "Adds new medical record")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully added new record",
+                    content = {
+                            @Content(mediaType = "application/json")})
+    })
     public ResponseEntity<MedicalRecord> addMedicalRecord(@RequestBody MedicalRecord medicalRecord) {
         return ResponseEntity.ok(medicalRecordService.createMedicalRecord(medicalRecord));
     }
--- risk-evaluator/src/main/java/com/medilabo/riskevaluator/controller/GlobalExceptionHandler.java
+++ risk-evaluator/src/main/java/com/medilabo/riskevaluator/controller/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.medilabo.riskevaluator.controller;
 
 import feign.FeignException;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Handles exceptions thrown when fetching data from microservices
@@ -16,6 +19,11 @@ public class GlobalExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "404", description = "Not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @ExceptionHandler(FeignException.class)
     public ResponseEntity<String> handleFeignException(FeignException ex) {
         logger.error(ex.getMessage());
